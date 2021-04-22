1070b28b5b7dbf78765f818e09c4c9f46efade9a
Cori Peck <cori.peck@ymail.com>
2021-04-22 17:35:18 -0500

    fix: completes code comments

---
+++ script/index.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector(".popup");
+const popupBtn = document.querySelector(".profile__edit-button");
+const form = document.querySelector(".form");
+const formBtn = document.querySelector(".form__close-btn");
+let currentName = document.querySelector(".profile__name").textContent;
+let currentJob = document.querySelector(".profile__occupation").textContent;
+
+function openPopup() {
+  document.querySelector("#fullName").value = currentName;
+  document.querySelector("#job").value = currentJob;
+
+  popup.classList.add("popup_status_opened");
+}
+
+function updateText() {
+  event.preventDefault();
+  let newName = document.querySelector("#fullName").value;
+  let newJob = document.querySelector("#job").value;
+
+  document.querySelector(".profile__name").textContent = newName;
+  document.querySelector(".profile__occupation").textContent = newJob;
+  event.preventDefault();
+  closePopup();
+}
+
+function closePopup() {
+  popup.classList.remove("popup_status_opened");
+}
+
+popupBtn.addEventListener("click", openPopup);
+form.addEventListener("submit", updateText);
+formBtn.addEventListener("click", closePopup);
